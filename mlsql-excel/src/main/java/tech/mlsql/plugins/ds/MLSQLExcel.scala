91e7052b8118af601b1917ff59a27e2d9134dbd3
WilliamZhu <allwefantasy@gmail.com>
2022-04-14 13:18:15 +0800

    update

--- mlsql-excel/src/main/java/tech/mlsql/plugins/ds/MLSQLExcel.scala
+++ mlsql-excel/src/main/java/tech/mlsql/plugins/ds/MLSQLExcel.scala
@@ -58,11 +58,12 @@ class MLSQLExcel(override val uid: String)
 
   override def shortFormat: String = "excel"
 
-  final val useHeader: Param[String] = new Param[String](this, "useHeader", "default false")
+  final val header: Param[String] = new Param[String](this, "header", "default false")
   final val dataAddress: Param[String] = new Param[String](this, "dataAddress", "Optional, default: \"A1\"; Currently the following address styles are supported:\n\nB3: Start cell of the data. Reading will return all rows below and all columns to the right. Writing will start here and use as many columns and rows as required.\nB3:F35: Cell range of data. Reading will return only rows and columns in the specified range. Writing will start in the first cell (B3 in this example) and use only the specified columns and rows. If there are more rows or columns in the DataFrame to write, they will be truncated. Make sure this is what you want.\n'My Sheet'!B3:F35: Same as above, but with a specific sheet.\nMyTable[#All]: Table of data. Reading will return all rows and columns in this table. Writing will only write within the current range of the table. No growing of the table will be performed. PRs to change this are welcome.")
-  final val treatEmptyValuesAsNulls: Param[String] = new Param[String](this, "treatEmptyValuesAsNulls", "Optional, default: true")
   final val inferSchema: Param[String] = new Param[String](this, "inferSchema", "Optional, default: false")
+  final val maxRowsInMemory: Param[String] = new Param[String](this, "maxRowsInMemory", "Optional, default None. If set, uses a streaming reader which can help with big files (will fail if used with xls format files)")
   final val workbookPassword: Param[String] = new Param[String](this, "workbookPassword", "Optional, default None. Requires unlimited strength JCE for older JVMs")
+  final val treatEmptyValuesAsNulls: Param[String] = new Param[String](this, "treatEmptyValuesAsNulls", "Optional, default: true")
   final val timestampFormat: Param[String] = new Param[String](this, "timestampFormat", "Optional, default: yyyy-mm-dd hh:mm:ss[.fffffffff]")
   final val sheetName: Param[String] = new Param[String](this, "sheetName", "Optional, For save excel")
 
