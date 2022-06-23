e1446b926fa7a79fb91e4a29dc08008632af3f78
WilliamZhu <allwefantasy@gmail.com>
2022-06-23 18:58:29 +0800

    update

--- mlsql-ext-ets/src/test/java/tech/mlsql/test/ETTest.scala
+++ mlsql-ext-ets/src/test/java/tech/mlsql/test/ETTest.scala
@@ -3,16 +3,20 @@ package tech.mlsql.test
 import org.apache.spark.streaming.SparkOperationUtil
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 import streaming.core.strategy.platform.SparkRuntime
+import tech.mlsql.plugins.ets.EmptyTable
 
 /**
  * 23/6/2022 WilliamZhu(allwefantasy@gmail.com)
  */
-class ETTest  extends FlatSpec with SparkOperationUtil with Matchers with BasicMLSQLConfig with BeforeAndAfterAll{
-  "kafka" should "work fine on spark" in {
-    withBatchContext(setupBatchContext(batchParams, "classpath:///test/empty.json")) { runtime: SparkRuntime =>
+class ETTest extends FlatSpec with SparkOperationUtil with Matchers with BasicMLSQLConfig with BeforeAndAfterAll {
+  "et" should "test-example" in {
+    withBatchContext(setupBatchContext(batchParams)) { runtime: SparkRuntime =>
       //执行sql
       implicit val spark = runtime.sparkSession
-
+      val et = new EmptyTable()
+      val df = spark.emptyDataFrame
+      val newDf = et.train(df, "", Map())
+      newDf.show()
 
     }
   }
