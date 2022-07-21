9593d3e2b4349327945162252dd826ad79270bdf
andiehuang <ckeys1993@gmail.com>
2022-07-21 11:10:04 +0800

    add filter for empty and null values

--- mlsql-mllib/src/main/java/tech/mlsql/plugins/mllib/ets/fe/SQLPatternDistribution.scala
+++ mlsql-mllib/src/main/java/tech/mlsql/plugins/mllib/ets/fe/SQLPatternDistribution.scala
@@ -3,14 +3,16 @@ package tech.mlsql.plugins.mllib.ets.fe
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import net.liftweb.json.{DefaultFormats, Extraction}
+import org.apache.spark.ml.param.Param
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions.{col, desc, udf}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import streaming.dsl.auth.TableAuthResult
-import streaming.dsl.mmlib.SQLAlg
+import streaming.dsl.mmlib.{Code, SQLAlg, SQLCode}
 import streaming.dsl.mmlib.algs.{Functions, MllibFunctions}
 import streaming.dsl.mmlib.algs.param.BaseParams
+import tech.mlsql.common.form.{Extra, FormParams, Input}
 import tech.mlsql.dsl.auth.ETAuth
 import tech.mlsql.dsl.auth.dsl.mmlib.ETMethod.ETMethod
 
@@ -25,14 +27,21 @@ import scala.collection.mutable
 class SQLPatternDistribution(override val uid: String) extends SQLAlg with MllibFunctions with Functions with BaseParams with ETAuth {
   def this() = this(BaseParams.randomUID())
 
+  var na_empty_filter = true
+  var internal_ch_limit = 1000
+
   def find_patterns(src: String): String = {
-    src.toSeq.map(c => c match {
+    val res = src.toSeq.map(c => c match {
       case p if c >= 'a' && c <= 'z' => 'a'
       case p if c >= 'A' && c <= 'Z' => 'A'
       case p if c >= '0' && c <= '9' => '9'
       case p if c >= 0x4E00 && c <= 0x9FA5 => 'A' // If the character is a Chinese, then retuen 'A'
       case _ => c
     }).toSeq.mkString("")
+    if (res.length > 1000)
+      res.substring(0, internal_ch_limit)
+    else
+      res
   }
 
   def is_need_conclude(freq_map: mutable.Map[String, Int], num_ch_upper: Int, num_ch_lower: Int, num_digit: Int): Boolean = {
@@ -121,7 +130,7 @@ class SQLPatternDistribution(override val uid: String) extends SQLAlg with Mllib
   }
 
   override def train(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
-    val limit = params.getOrElse("limit", "1000").toInt
+    val limit = params.getOrElse(limit_num.name, "100").toInt
     var pattern_func = udf(find_patterns(_))
     var pattern_func1 = udf(find_alternativePatterns(_))
     val rows_num = df.count()
@@ -129,7 +138,12 @@ class SQLPatternDistribution(override val uid: String) extends SQLAlg with Mllib
       var col_pattern_map = Map[String, String]()
       sc.dataType match {
         case StringType =>
-          val res = df.withColumn("pattern", pattern_func(col(sc.name))).withColumn("alternativePattern", pattern_func1(col(sc.name)))
+          val sub_df = na_empty_filter match {
+            case true => df.select(sc.name).where(col(sc.name).isNotNull).where(col(sc.name) =!= "")
+            case _ => df.select(sc.name)
+          }
+          require(!sub_df.isEmpty, s"Please make sure the column ${sc.name} contains content except null or empty content!")
+          val res = sub_df.withColumn("pattern", pattern_func(col(sc.name))).withColumn("alternativePattern", pattern_func1(col(sc.name)))
           val pattern_group_df = res.groupBy(col("pattern"), col("alternativePattern")).count().orderBy(desc("count")).withColumn("ratio", col("count") / rows_num.toDouble)
           val total_count = pattern_group_df.count()
           val res_json_str = pattern_group_df.limit(limit).toJSON.collectAsList.toString
@@ -147,9 +161,56 @@ class SQLPatternDistribution(override val uid: String) extends SQLAlg with Mllib
     spark.createDataFrame(spark.sparkContext.parallelize(res, 1), StructType(Seq(StructField("columnName", StringType), StructField("patternDistribution", StringType))))
   }
 
-  override def load(sparkSession: SparkSession, path: String, params: Map[String, String]): Any = ???
+  override def load(sparkSession: SparkSession, path: String, params: Map[String, String]): Any = {
+    throw new RuntimeException(s"${getClass.getName} not support load function.")
+  }
 
-  override def predict(sparkSession: SparkSession, _model: Any, name: String, params: Map[String, String]): UserDefinedFunction = ???
+  override def predict(sparkSession: SparkSession, _model: Any, name: String, params: Map[String, String]): UserDefinedFunction = {
+    throw new RuntimeException(s"${getClass.getName} not support predict function.")
+  }
 
   override def auth(etMethod: ETMethod, path: String, params: Map[String, String]): List[TableAuthResult] = ???
+
+  final val limit_num: Param[String] = new Param[String](this, "limit",
+    FormParams.toJson(Input(
+      name = "limit",
+      value = "",
+      extra = Extra(
+        doc =
+          """
+            | define the amounts of patterns are shown
+            | e.g. limit = "100"
+          """,
+        label = "limit",
+        options = Map(
+          "valueType" -> "string",
+          "defaultValue" -> "100",
+          "required" -> "false",
+          "derivedType" -> "NONE"
+        )), valueProvider = Option(() => {
+        ""
+      })
+    )
+    )
+  )
+
+  override def codeExample: Code = Code(SQLCode,
+    """
+      |
+      |set abc='''
+      |{"name": "elena", "age": 57, "phone": 15552231521, "income": 433000, "label": 0}
+      |{"name": "candy", "age": 67, "phone": 15552231521, "income": 1200, "label": 0}
+      |{"name": "bob", "age": 57, "phone": 15252211521, "income": 89000, "label": 0}
+      |{"name": "candy", "age": 25, "phone": 15552211522, "income": 36000, "label": 1}
+      |{"name": "candy", "age": 31, "phone": 15552211521, "income": 300000, "label": 1}
+      |{"name": "finn", "age": 23, "phone": 15552211521, "income": 238000, "label": 1}
+      |''';
+      |
+      |load jsonStr.`abc` as table1;
+      |select name, age, income from table1 as table2;
+      |run table2 as PatternDistribution.`` as pd_table;
+      |;
+    """.stripMargin)
+
+  setDefault(limit_num, "100")
 }
