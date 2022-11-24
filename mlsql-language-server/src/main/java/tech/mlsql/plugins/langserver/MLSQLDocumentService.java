5a9b8128dfe871158a1dc47395b01b624731ad56
WilliamZhu <allwefantasy@gmail.com>
2022-11-24 09:40:51 +0800

    fix autosuggest error

--- mlsql-language-server/src/main/java/tech/mlsql/plugins/langserver/MLSQLDocumentService.java
+++ mlsql-language-server/src/main/java/tech/mlsql/plugins/langserver/MLSQLDocumentService.java
@@ -6,9 +6,10 @@ import org.eclipse.lsp4j.*;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import org.eclipse.lsp4j.services.TextDocumentService;
 import tech.mlsql.autosuggest.statement.SuggestItem;
-import tech.mlsql.common.utils.base.Joiner;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -44,39 +45,40 @@ public class MLSQLDocumentService implements TextDocumentService {
     public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion(CompletionParams position) {
         String uri = position.getTextDocument().getUri();
         String sql = fileTracker.getText(uri);
-        List<String> finalTextList = new ArrayList<>();
-        int increment = 0;
+
+//        List<String> finalTextList = new ArrayList<>();
+//        int increment = 0;
         //vscode-notebook-cell
         // #ch0000000
-        if (uri.startsWith("vscode-notebook-cell")) {
-            List<String> cells = new ArrayList<>();
-            String[] temp = uri.split("#ch");
-            String prefix = temp[0];
-            for (String tempUri : fileTracker.getOpenFiles()) {
-                if (tempUri.startsWith(prefix)) {
-                    cells.add(tempUri);
-                }
-            }
-            Collections.sort(cells);
-            for (String cell : cells) {
-                String rawText = fileTracker.getText(cell);
-                if (cell.trim().equals(uri.trim())) {
-                    finalTextList.add(sql);
-                    break;
-                }
-                increment += rawText.split("\n").length;
-                finalTextList.add(rawText);
-            }
-        } else {
-            finalTextList.add(sql);
-        }
+//        if (uri.startsWith("vscode-notebook-cell")) {
+//            List<String> cells = new ArrayList<>();
+//            String[] temp = uri.split("#ch");
+//            String prefix = temp[0];
+//            for (String tempUri : fileTracker.getOpenFiles()) {
+//                if (tempUri.startsWith(prefix)) {
+//                    cells.add(tempUri);
+//                }
+//            }
+//            Collections.sort(cells);
+//            for (String cell : cells) {
+//                String rawText = fileTracker.getText(cell);
+//                if (cell.trim().equals(uri.trim())) {
+//                    finalTextList.add(sql);
+//                    break;
+//                }
+//                increment += rawText.split("\n").length;
+//                finalTextList.add(rawText);
+//            }
+//        } else {
+//            finalTextList.add(sql);
+//        }
 
         Map<String, String> initParams = LSContext.initParams;
 
-        String finalSql = Joiner.on("\n").join(finalTextList);
+        String finalSql = sql;
         Map<String, String> params = new HashMap<>();
         params.put("sql", finalSql);
-        params.put("lineNum", (position.getPosition().getLine() + 1 + increment) + "");
+        params.put("lineNum", (position.getPosition().getLine() + 1) + "");
         params.put("columnNum", position.getPosition().getCharacter() + "");
         String engineURL = initParams.getOrDefault("engine.url", "http://127.0.0.1:9003");
         if (engineURL.startsWith("/")) {
