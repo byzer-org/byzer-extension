45d356b1cd975366b7e7368eb7a84c9324c2b6cb
WilliamZhu <allwefantasy@gmail.com>
2023-06-23 12:58:12 +0800

    update

--- byzer-llm/src/main/java/tech/mlsql/plugins/llm/custom/SFT.scala
+++ byzer-llm/src/main/java/tech/mlsql/plugins/llm/custom/SFT.scala
@@ -3,6 +3,7 @@ package tech.mlsql.plugins.llm.custom
 import org.apache.spark.sql.DataFrame
 import streaming.dsl.ScriptSQLExec
 import tech.mlsql.common.utils.log.Logging
+import tech.mlsql.common.utils.serder.json.JSONTool
 import tech.mlsql.ets.Ray
 
 /**
@@ -11,25 +12,16 @@ import tech.mlsql.ets.Ray
 class SFT(params: Map[String, String]) extends Logging {
   def run(): DataFrame = {
     val session = ScriptSQLExec.context().execListener.sparkSession
-    val localModelDir = params.getOrElse("localModelDir", "")
-    val localPathPrefix = params.getOrElse("localPathPrefix", "/tmp")
     val trainer = new Ray()
-
-    val learningRate = params.getOrElse("learningRate", "5e-5")
-    val finetuningType = params.getOrElse("finetuningType", "lora")
-    val numTrainEpochs = params.getOrElse("numTrainEpochs", "1")
-
-
     val devices = params.getOrElse("devices", "-1")
-    val maxSteps = params.getOrElse("maxSteps", "100")
-    val saveSteps = params.getOrElse("saveSteps", "50")
+    val pretrainedModelType = params.getOrElse("pretrainedModelType", "custom/baichuan")
+    val realPretrainedModelType = pretrainedModelType.split("/").last
+    val train_params = JSONTool.toJsonStr(params)
 
-
-    val pretrainedModel = params.getOrElse("pretrainedModel", "chatglm6b")
-    
     val code =
       s"""
          |import os
+         |import json
          |if ${devices} != -1:
          |    os.environ["CUDA_VISIBLE_DEVICES"] = "${devices}"
          |try:
@@ -49,41 +41,12 @@ class SFT(params: Map[String, String]) extends Logging {
          |except ImportError:
          |    pass
          |
-         |from pyjava.api.mlsql import RayContext
-         |import json
-         |import uuid
-         |import shutil
-         |import byzerllm
-         |from byzerllm.${pretrainedModel} import sft
+         |from pyjava import RayContext
+         |from byzerllm.${realPretrainedModelType} import sft_train
          |
-         |ray_context = RayContext.connect(globals(),None)
-         |
-         |rd=str(uuid.uuid4())
-         |
-         |MODEL_DIR=os.path.join("${localPathPrefix}",rd,"pretrained_model")
-         |OUTPUT_DIR=os.path.join("${localPathPrefix}",rd,"finetune_model")
-         |DATA_DIR=os.path.join("${localPathPrefix}",rd,"finetune_data")
-         |DATA_FILE=os.path.join(DATA_DIR,"data.jsonl")
-         |
-         |if not os.path.exists(DATA_DIR):
-         |    os.makedirs(DATA_DIR)
-         |
-         |if not "${localModelDir}":
-         |    restore_model(ray_context.conf(),MODEL_DIR)
-         |else:
-         |    MODEL_DIR = "${localModelDir}"
-         |
-         |with open(DATA_FILE,"w") as f:
-         |    for line in ray_context.collect():
-         |        s = json.dumps(line,ensure_ascii=False)
-         |        f.write(s+"\\n")
-         |
-         |new_model_dir=os.path.join(OUTPUT_DIR,"final")
-         |
-         |new_dir_path = f"{new_model_dir}/pretrained_model"
-         |shutil.copytree(MODEL_DIR, new_dir_path)
-         |
-         |model_binary = byzerllm.load_model(new_model_dir)
+         |ray_context = RayContext.connect(globals(),context.conf["rayAddress"])
+         |train_params = json.loads('''${train_params}''')
+         |model_binary = sft_train(ray_context.data_servers(),train_params,ray_context.conf())
          |ray_context.build_result(model_binary)""".stripMargin
     logInfo(code)
     trainer.train(session.emptyDataFrame, "", Map(
